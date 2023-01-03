5fbc7023cd152264e3d5755dff0a04e5696bde49
Savitur Maharaj <saviturmaharaj@gmail.com>
2023-01-02 23:08:58 -0700

    Complete game

--- script.js
+++ script.js
@@ -36,3 +36,48 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("Enter Rock, Paper, or Scissors:");
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection)
+        let winStatus = winPosition(roundResult);
+        let loseStatus = losePosition(roundResult);
+
+        if (winStatus){
+            playerScore += 1;
+        }else if (loseStatus){
+            computerScore += 1;
+        }
+        console.log(roundResult)
+        console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
+
+        if (i === 4){
+            if (playerScore > computerScore){
+                console.log("Player wins!")
+            }else{
+                console.log("Computer wins")
+            }
+        }
+    }
+}
+
+function winPosition(roundResult){
+    let position = roundResult.search("Win");
+    if (position === -1){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function losePosition(roundResult){
+    let position = roundResult.search("Lose");
+    if (position === -1){
+        return false;
+    }else{
+        return true;
+    }
+}
